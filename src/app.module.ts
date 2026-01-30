0708307d01c53f5c211ce224b9ee3830d1f488fd
joabesimao <simaojoabecosta@gmail.com>
2026-01-30 00:33:47 -0300

    feat: ensure middleware for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ProductsModule } from './infra/nest/modules/product-module';
-import { PrismaModule } from './infra/nest/modules/prisma-module';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+
+import { AuthMiddleware } from './auth/auth.middleware';
 import { AuthModule } from './infra/nest/modules/auth-module';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from './infra/nest/modules/prisma-module';
+import { ProductsModule } from './infra/nest/modules/product-module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, AuthModule],
+  imports: [PrismaModule, ProductsModule, JwtModule, AuthModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('/products');
+  }
+}
